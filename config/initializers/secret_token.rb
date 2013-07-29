40ec5bde53bc3d45d320ed89b91408c9b11badaf
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-29 16:04:46 -0700

    all done except template error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogIdea::Application.config.secret_key_base = 'de7c1e851d9f32162cf7ae20c8e83af4fdbc6ccd42332b6a82fea27923f4dc35ad9b4da7ce7509d486fc1653bff15371b68f2ffc64c62fee8cf3344c48a6ef8a'
