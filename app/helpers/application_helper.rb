8bc094e786d1aeb2e12bf9b1bee6ff3de21ea586
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-08 21:37:01 -0700

    added timestamp on posts and clock on welcome index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+def timeago(time)
+    time_ago = ((Time.now - time) / 86400) # returns a number in days
+    # Less than a day ago
+    if time_ago < 1
+      time_ago *= 24
+    # Less than an hour ago
+      if time_ago < 1
+        time_ago *= 60
+    # Less than a minute ago
+        if time_ago < 1
+          time_ago *= 60
+          return "#{time_ago = time_ago.round}" + ((time_ago == 1) ? " second ago" :  " seconds ago")
+        end
+        return "#{time_ago = time_ago.round}" + ((time_ago == 1) ? " minute ago" :  " minutes ago")
+      end
+      return "#{time_ago = time_ago.round}" + ((time_ago == 1) ? " hour ago" :  " hours ago")
+    elsif time_ago == 1
+      return "#{time_ago = time_ago.round} day ago"
+    else
+      return "#{time_ago = time_ago.round} days ago"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130809021556) do
+ActiveRecord::Schema.define(version: 20130809032448) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20130809021556) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
+
+  create_table "post_translations", force: true do |t|
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
